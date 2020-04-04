fb034439fecf254caec3ffecedf1fcf010a12218
kaleshkannan <kaleshkannan41@gmail.com>
2020-04-04 20:18:29 +0530

    navigation update

--- routes/appRoute.js
+++ routes/appRoute.js
@@ -2,10 +2,10 @@ const express   		= require('express');
 const route 			= express.Router();
 const appController		= require('../controllers/appController');
 
-
-route.get('/index', appController.index);
-route.get('/chat', appController.chat);
-route.get('/file', appController.file);
+route.get('', appController.index);
+route.get('/app/index', appController.index);
+route.get('/app/chat', appController.chat);
+route.get('/app/file', appController.file);
 
 
 module.exports = route;
